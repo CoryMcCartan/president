e9743041b05e3e69d845c89eda48765e5daf49e3
Cory McCartan <cory.mccartan@gmail.com>
2020-05-20 11:40:46 -0400

    Run.

--- model/tweets.R
+++ model/tweets.R
@@ -9,7 +9,9 @@ raw_tweets = read_rds("data/tweets.rdata") %>%
     select(tstamp=created_at, rt=is_retweet) %>%
     mutate(tstamp = with_tz(mdy_hms(tstamp, tz="UTC"), "America/New_York"),
            hour = (hour(tstamp) + 12) %% 24,
-           date = force_tz(as_datetime(as_date(tstamp)+1),
+           date = as_date(tstamp),
+           date = if_else(hour >= 12, date - 1, date),
+           date = force_tz(as_datetime(date+1),
                            "America/New_York") - 12*3600,
            rt = coalesce(rt, F))
 
@@ -36,7 +38,9 @@ tweets = full_join(tweets, missing, by=c("date", "hour")) %>%
     mutate(n_cuml = cumsum(n),
            n_rem = sum(n) - n_cuml) %>%
     ungroup %>%
-    mutate(n_h1 = slide_vec(n, sum, .before=1),
+    mutate(hr_rem = 7*24 - ((day - 1)*24 + hour),
+           pace = hr_rem * (n_cuml + 0.5) / ((day - 1)*24 + hour + 0.5),
+           n_h1 = slide_vec(n, sum, .before=1),
            n_h6 = slide_vec(n, sum, .before=6),
            n_h12 = slide_vec(n, sum, .before=12),
            n_d1 = slide_vec(n, sum, .before=1*24),
@@ -86,26 +90,48 @@ r2_d = function(d) {
 }
 map_dbl(1:7, r2_d)
 
-m = lm(log(n_rem+1) ~ log(n_cuml+1)*hour + log(n_h1+1) + log(n_h6+1) +
+m = lm(log(n_rem+1) ~ log(n_cuml+1) + log(n_h1+1) + log(n_h6+1) +
             log(n_h12+1) + log(n_d1+1) + log(n_d2+1) + log(n_d3) +
             log(n_d5) +  log(n_d7) + log(n_d10) + log(n_w2) + log(n_w3) +
             log(n_w4) + log(n_w6) + log(n_w8) + rt_d7 + rt_w8 + I(hour^2) +
-            as.factor(day)*hour,
+            as.factor(day)*hour + log(pace),# + (1|week),
        data=d_train)#,
        #prior=cauchy(), chains=1)
 #y_pred = colMeans(posterior_predict(m, newdata=d_test))
-d_test$y_pred = exp(predict(m, newdata=d_test)) - 1
-y_act = d_test$n_rem
-sd(y_pred - y_act)
-cor(y_pred, y_act)^2
-median(abs(y_pred - y_act))
+d_test$y_pred = exp(predict(m, newdata=d_test, allow.new.levels=T)) - 1
+d_test$y_pred = predict(m, newdata=d_test, type="response")
+with(d_test, sd(y_pred - n_rem))
+with(d_test, cor(y_pred, n_rem)^2)
+with(d_test, median(abs(y_pred - n_rem)))
 
 d_test %>%
     mutate(error = y_pred - n_rem) %>%
     group_by(day, hour) %>%
-    summarize(se=sd(error), mad=mean(abs(error)), rsq=100*cor(n_cuml+n_rem, n_cuml+y_pred)^2) %>%
-    pivot_longer(se:rsq) %>%
-ggplot(aes(day+hour/24, value, color=name)) + geom_line()
+    summarize(se=sd(error), mad=mean(abs(error)),
+              rsq=100*cor(n_rem, y_pred)^2,
+              rsq_ref = 100*cor(n_cuml, n_rem)^2) %>%
+    pivot_longer(-day:-hour) %>%
+ggplot(aes(day+hour/24, value, color=name)) + geom_line(size=1)
+
+
+select(tail(d_test, 20), date, day, hour, n, n_cuml, n_rem, y_pred, hr_rem, pace) %>%
+    mutate(date = date + hour*3600,
+           wk  = n_cuml + y_pred)
+
+d_test %>%
+    mutate(error = y_pred - n_rem) %>%
+ggplot(aes(error)) +
+    facet_wrap(~ day) +
+    geom_histogram(bins=10)
+
+m = glm(n_rem ~ n_cuml + n_h1 + n_h6 +
+            n_h12 + n_d1 + n_d2 + n_d3 +
+            n_d5 +  n_d7 + n_d10 + n_w2 + n_w3 +
+            n_w4 + n_w6 + n_w8 + rt_d7 + rt_w8 + I(hour^2) +
+            as.factor(day)*hour + pace,
+       data=d_train,
+       family=quasipoisson())
+
 
 
 x_train = model.matrix(form, data=d_train)
